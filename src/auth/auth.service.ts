8af2dd64d37516674756836be5a70e8d7ca985e1
bonv <toitaigioi2402@gmail.com>
2025-08-25 09:30:41 +0700

    init proj 2

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService{
+    login() {
+        return {
+            msg: 'I have signed up'
+        }
+    }
+
+    signup() {
+        return {
+            msg: 'I have signed in'
+        }
+    }
+}
